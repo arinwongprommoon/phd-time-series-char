257ee7d18b0aba2ac83b82b48d4b00211a5fdb89
arinwongprommoon <arin.wongprommoon@ed.ac.uk>
2023-08-31 19:51:32 +0100

    refactor: umap_grid to script & functions to src

---
+++ scripts/script_umap_grid.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from matplotlib.backends.backend_pdf import PdfPages
+
+from src.umapper.umapper import umap_grid
+from src.viz.grid import plot_umap_grid
+
+hyperparam_dict = {
+    "n_neighbors": [5, 10],
+    "min_dist": [0.25, 0.5],
+}
+
+
+# TODO: Make this script less reliant on these CSV files
+features_scaled = pd.read_csv(
+    "../data/processed/features_scaled.csv", index_col=[0, 1, 2]
+)
+labels_df = pd.read_csv("../data/processed/labels_df.csv", index_col=[0, 1, 2])
+
+position_list = features_scaled.index.get_level_values("position").to_list()
+strain_list = [position.split("_")[0] for position in position_list]
+strain_relabel_lookup = {
+    "tsa1tsa2morgan": "tsa1Δ tsa2Δ",
+    "by4742swain": "BY4742",
+}
+strain_list = [strain_relabel_lookup.get(item, item) for item in strain_list]
+
+common_idx = features_scaled.index.intersection(labels_df.index)
+scores_list = labels_df.loc[common_idx].score.to_list()
+scores_relabel_lookup = {
+    0: "Oscillatory",
+    1: "Non-oscillatory",
+}
+scores_list = [scores_relabel_lookup.get(item, item) for item in scores_list]
+
+label_list = []
+for strain, score in zip(strain_list, scores_list):
+    if score == "Non-oscillatory":
+        label_list.append(score)
+    elif score == "Oscillatory":
+        label_list.append(strain)
+
+
+label_palette_map = {
+    "Non-oscillatory": "lightgrey",
+    "tsa1Δ tsa2Δ": "C0",
+    "BY4742": "C1",
+}
+
+embedding_array = umap_grid(hyperparam_dict, features_scaled)
+plot_umap_grid(
+    hyperparam_dict=hyperparam_dict,
+    embedding_array=embedding_array,
+    hue=label_list,
+    palette=label_palette_map,
+)
+
+# Save figures
+pdf_filename = "../reports/umap_grid_st01235.pdf"
+with PdfPages(pdf_filename) as pdf:
+    for fig in range(1, plt.gcf().number + 1):
+        pdf.savefig(fig)
+# Close all figures
+plt.close("all")
--- src/viz/grid.py
+++ src/viz/grid.py
@@ -2,29 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import umap
-
-
-def umap_grid(hyperparam_dict, features_scaled, random_state=69):
-    n_neighbors_list = hyperparam_dict["n_neighbors"]
-    min_dist_list = hyperparam_dict["min_dist"]
-    x_dim = len(n_neighbors_list)
-    y_dim = len(min_dist_list)
-    embedding_array = np.zeros(shape=(x_dim, y_dim), dtype="object")
-
-    for x_index, n_neighbors in enumerate(n_neighbors_list):
-        for y_index, min_dist in enumerate(min_dist_list):
-            reducer = umap.UMAP(
-                random_state=random_state,
-                n_neighbors=n_neighbors,
-                min_dist=min_dist,
-                n_components=2,
-            )
-            mapper = reducer.fit(features_scaled)
-            embedding = mapper.embedding_
-            embedding_array[x_index, y_index] = embedding
-
-    return embedding_array
+import seaborn as sns
 
 
 def plot_umap_grid(
