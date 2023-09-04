392d51033d13908f50e1d4ea1d3987c8b141d4e7
arinwongprommoon <arin.wongprommoon@ed.ac.uk>
2023-09-04 14:34:26 +0100

    feat(src/ml): fft transformer
    WHY IS THIS CHANGE NEEDED?:
    - evaluate fft as featurisation

--- src/ml/transformers.py
+++ src/ml/transformers.py
@@ -3,6 +3,7 @@
 # Wrap post-processes into objects that scikit-learn can make a pipeline from
 
 from postprocessor.core.processes.catch22 import catch22, catch22Parameters
+from postprocessor.core.processes.fft import fft, fftParameters
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
@@ -18,3 +19,18 @@ class Catch22Transformer(BaseEstimator, TransformerMixin):
     def transform(self, x):
         catch22_runner = catch22(catch22Parameters.default())
         return catch22_runner.run(x)
+
+
+class FFTTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, x, y=None):
+        self.x = x
+        self.y = y
+        return self
+
+    def transform(self, x):
+        fft_runner = fft(fftParameters.default())
+        _, power = fft_runner.run(x)
+        return power
