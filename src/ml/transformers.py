cf5e232b772d3e7b946d2175a781f3b5175227d4
arinwongprommoon <arin.wongprommoon@ed.ac.uk>
2023-09-04 10:42:04 +0100

    remove(src/ml): detrend transformer
    WHY IS THIS CHANGE NEEDED?:
    - not used

--- src/ml/transformers.py
+++ src/ml/transformers.py
@@ -3,23 +3,7 @@
 # Wrap post-processes into objects that scikit-learn can make a pipeline from
 
 from postprocessor.core.processes.catch22 import catch22, catch22Parameters
-from postprocessor.core.processes.detrend import detrend, detrendParameters
 from sklearn.base import BaseEstimator, TransformerMixin
-
-
-class DetrendTransformer(BaseEstimator, TransformerMixin):
-    def __init__(self, window=45):
-        self.window = window
-
-    def fit(self, x, y=None):
-        self.x = x
-        self.y = y
-        return self
-
-    def transform(self, x, y=None):
-        detrend_params = detrendParameters(self.window)
-        detrend_runner = detrend(detrend_params)
-        return detrend_runner.run(x)
 
 
 class Catch22Transformer(BaseEstimator, TransformerMixin):
